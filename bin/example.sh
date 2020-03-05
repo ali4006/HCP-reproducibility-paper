1e52e425095603023e3e0f019831164fa010dd1e
ali4006 <ali.salar007@gmail.com>
2020-03-05 13:24:10 -0500

    update example script, add new pg figures, add bin data samples

--- bin/example.sh
+++ bin/example.sh
@@ -11,9 +11,16 @@ input_image=$1
 base_name=$(basename ${input_image} .nii.gz)
 bet_output="${base_name}_brain.nii.gz"
 bet_output_binarized="${base_name}_brain_bin.nii.gz"
+robustfov_output="${base_name}_roi.nii.gz"
+
+# Cropping to remove neck and lower head
+robustfov -i ${input_image} -r ${robustfov_output}
 
 # Run FSL bet, put result in ${bet_output}
 bet ${input_image} ${bet_output}
+echo "Voxels / volume in brain mask:"
+fslstats ${bet_output} -V
+bet ${robustfov_output} ${bet_output}
 echo "Voxels / volume in brain mask:"
 fslstats ${bet_output} -V
 
@@ -23,4 +30,4 @@ echo "Voxels / volume in binarized brain mask:"
 fslstats ${bet_output_binarized} -V
 
 # Remove temporary file
-\rm ${bet_output}
+\rm ${robustfov_output}
