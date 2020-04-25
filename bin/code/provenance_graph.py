38b1533db35fd6753b565ac7637014bc1702be31
ali4006 <ali.salar007@gmail.com>
2020-04-24 23:48:09 -0400

    update Fig.3 and Fig.1

--- bin/code/provenance_graph.py
+++ bin/code/provenance_graph.py
@@ -50,6 +50,45 @@ def get_opened_files_list(openfile_cursor, pid):
     return openfile_cursor.fetchall()
 
 
+def add_edge(graph_, from_, to_, mode):
+    if mode == 'file':
+        graph_.attr('edge', style='solid',color='black', penwidth='1')
+    elif mode == 'ptree':
+        graph_.attr('edge', style='dashed', color='grey', penwidth='1')
+    graph_.edge(str(from_), str(to_))
+    return graph_
+
+
+def add_node(graph_, id, name, mode, color):
+    # with graph_.subgraph(name='cluster_{}'.format(cluster)) as ngraph_:
+    if mode == 'process':
+        if color == 'red':
+            if name == 'fslmaths':
+                name = 'fsl\nmaths'
+            if name == 'new_invwarp':
+                name = 'new\ninvwarp'
+            graph_.node(str(id), name.upper(), width='1.5', fontsize='16',fontname="Helvetica bold",
+                        fontcolor='black', shape='circle',
+                        style='filled', fillcolor='#EDB9B8', penwidth='1')
+        elif color == 'green':
+            graph_.node(str(id), name, width='1.5', penwidth='1', fontsize='16', shape='circle',
+                        style='filled', fillcolor='#7FCDB1', fontname="Helvetica bold")
+        elif color == 'subgreen':
+            graph_.node(str(id), name, width='1.5', fontsize='16',fontname="Helvetica bold",
+                        shape='circle', style='filled', fillcolor='#7FCDB1')
+        elif color == 'white':
+            graph_.node(str(id), name, width='1.5', shape='circle', style='filled', fontsize='16',
+                        fontname="Helvetica bold", fillcolor='white')
+    elif mode == 'file':
+        if name != '':
+            graph_.node(str(id), name, shape='box', style='filled', fillcolor=color, penwidth='1',
+                        fontsize='16', fontname="Helvetica bold")
+        else:
+            graph_.node(str(id), name, shape='box', style='filled', fillcolor=color, penwidth='1')
+    # graph_.node(str(id), name) 
+    return graph_
+
+
 def create_provenance_graph(db_path, pid, graph_, pipe_list=[], multi_list={},
                             tmp_list=[], total_proc_diff=[], dag=False):
     try:
@@ -64,21 +103,32 @@ def create_provenance_graph(db_path, pid, graph_, pipe_list=[], multi_list={},
     # Get the process name
     process_name = get_the_processes_name(executed_cursor, pid)
 
-    if "/usr/bin" not in process_name and process_name != "":
-        # Get the list of opened files (w/r) from pid
-        process_ofiles = get_opened_files_list(openfile_cursor, pid)
+    short_name = {}
+    short_name['ACPCAlignment.sh'] = 'ACPC-A'
+    short_name['BrainExtraction_FNIRTbased.sh'] = 'BExt'
+    short_name['AnatomicalAverage.sh'] = 'AAve'
+    short_name['BiasFieldCorrection_sqrtT1wXT1w.sh'] = 'BFC'
+    short_name['T2wToT1wDistortionCorrectAndReg.sh'] = 'DC'
+    short_name['AtlasRegistrationToMNI152_FLIRTandFNIRT.sh'] = 'AR'
 
+    filter_ = re.match('(/usr/bin/)|(/bin/)', process_name)
+    exclude_list = [""]
+    # exclude_list = ["", "imtest", "imcp", "remove_ext",
+    #                 "fslval", "avscale", "fslhd"]
+    if filter_ is None and process_name.split('/')[-1:][0] not in exclude_list:
+        # Get the list of opened files (w/r) from pid
+        flag_ = False
+        process_ofiles = get_opened_files_list(openfile_cursor, pid)
         p_name = process_name.split('/')[-1:][0]
-        # graph_.attr('node', shape='circle', style='filled', fillcolor='white')
         if pid in total_proc_diff:
-            graph_.node(str(pid), ''.join([str(pid), '#', p_name]),
-                        shape='circle', style='filled', fillcolor='red')
+            graph_ = add_node(graph_, pid, p_name, 'process', 'red')
+            flag_ = True
         elif total_proc_diff != []:
-            graph_.node(str(pid), ''.join([str(pid), '#', p_name]),
-                        shape='circle', style='filled', fillcolor='green')
+            graph_ = add_node(graph_, pid, p_name, 'process', 'green')
+            if p_name in short_name.keys():
+                graph_ = add_node(graph_, pid, short_name[p_name], 'process', 'subgreen')
         else:
-            graph_.node(str(pid), ''.join([str(pid), '#', p_name]),
-                        shape='circle', style='filled', fillcolor='white')
+            graph_ = add_node(graph_, pid, p_name, 'process', 'white')
 
         for file in process_ofiles:
             fname_ = str(os.path.basename(file[1]))
@@ -99,32 +149,32 @@ def create_provenance_graph(db_path, pid, graph_, pipe_list=[], multi_list={},
             hex_dig_file = hash_object.hexdigest()
             # Read files
             if file[2] == 1 and str(hex_dig_file) in pipe_list:
-                graph_.attr('edge', style='solid')
-                graph_.edge(str(hex_dig_file), str(pid))
+                graph_ = add_edge(graph_, hex_dig_file, pid, 'file')
 
             # Write files
             elif file[2] == 2 and (fname_ in pipe_list or fname_ in tmp_list):
                 if str(hex_dig_file) not in pipe_list:
                     pipe_list.append(str(hex_dig_file))
-                graph_.attr('node', shape='box', style='filled', fillcolor='white')
+                color = 'white'
                 if dag and (fname_ in tmp_list or fname_ in multi_list.keys()):
-                    graph_.attr('node', style='filled', fillcolor='grey')
+                    color = 'grey'
                 elif fname_ in tmp_list or fname_ in multi_list.keys():
-                    graph_.attr('node', style='filled', fillcolor='grey')
-                graph_.node(str(hex_dig_file), ''.join([str(pid), '#', fname_]))
-                graph_.attr('edge', style='solid')
-                graph_.edge(str(pid), str(hex_dig_file))
+                    color = 'grey'
+                if flag_:
+                    graph_ = add_node(graph_, hex_dig_file, fname_, 'file', color)
+                else:
+                    graph_ = add_node(graph_, hex_dig_file, fname_, 'file', color)
+                graph_ = add_edge(graph_, pid, hex_dig_file, 'file')
 
     for child in child_list:
         p_name_child = get_the_processes_name(executed_cursor, child)
         # find node in graph
+        filter_ = re.match('(/usr/bin/)|(/bin/)', p_name_child)
         x_node = [True for v in graph_.body if '\t{} '.format(pid) in v]
-        if "/usr/bin" not in p_name_child and x_node and p_name_child != "":
-            graph_.attr('edge', style='dashed')
-            graph_.edge(str(pid), str(child))
+        if filter_ is None and x_node and p_name_child.split('/')[-1:][0] not in exclude_list:
+            graph_ = add_edge(graph_, pid, child, 'ptree')
         create_provenance_graph(db_path, child, graph_, pipe_list,
                                 multi_list, tmp_list, total_proc_diff, dag)
-
 
 def parse_transient(tr_file):
     try:
@@ -200,7 +250,7 @@ def main(args=None):
             lst_files.append(elem)
     # Create provenance graphs
     graph = Di('Graph',
-               filename='images/p-graph',
+               filename='figures/p-graph',
                format='pdf',
                strict=True)
     # graph.attr(compound=True)
@@ -209,7 +259,7 @@ def main(args=None):
     graph.render()
 
     graph = Di('Graph',
-               filename='images/p-graph-dag',
+               filename='figures/p-graph-dag',
                format='pdf',
                strict=True)
     create_provenance_graph(db_file, 1, graph, lst_files, multi_list,
@@ -217,7 +267,7 @@ def main(args=None):
     graph.render()
 
     graph = Di('Graph',
-               filename='images/p-graph-dag-labelled',
+               filename='figures/p-graph-dag-labelled',
                format='pdf',
                strict=True)
     create_provenance_graph(db_file, 1, graph, lst_files, multi_list,
